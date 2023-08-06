09e01ef3fa5fb764439ed982feff9f0ba69b22a5
Ahammed rufaid <58688964+rufaidem@users.noreply.github.com>
2023-08-07 03:40:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
