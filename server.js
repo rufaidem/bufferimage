8bc856bb5f4d2a0af82d5a805b9f7cada9b450ae
Ahammed rufaid <58688964+rufaidem@users.noreply.github.com>
2023-08-07 03:31:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,57 +2,30 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const { parse } = require('querystring');
-const FormData = require('form-data');
+const formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-    // Serve the HTML file
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    // Serve the index.html file
+    const indexPath = path.join(__dirname, 'index.html');
+    fs.readFile(indexPath, 'utf8', (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else if (req.method === 'POST' && req.url === '/upload') {
-    let body = '';
-
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      const formData = parse(body);
-      const imageFile = req.headers['x-file-name'];
-      
-      if (!imageFile) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end('Bad Request');
         return;
       }
-
-      const imagePath = path.join(__dirname, imageFile);
-      const imageBuffer = Buffer.from(formData.avatar, 'base64');
-
-      fs.writeFile(imagePath, imageBuffer, (err) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Image uploaded and saved');
-        }
-      });
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
     });
+  } else if (req.method === 'POST' && req.url === '/upload') {
+    // ... rest of the code for handling image upload
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.end('404 Not Found');
   }
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
